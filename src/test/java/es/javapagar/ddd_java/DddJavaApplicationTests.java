8b99a566b6c49c799cac053ecc7aa1e73add690c
javapagar <apariciogarcia.j@gmail.com>
2024-07-16 18:49:32 +0200

    First commit

---
+++ src/test/java/es/javapagar/ddd_java/DddJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.javapagar.ddd_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
